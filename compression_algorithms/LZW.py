96fef010fd8fbf8b3b7ae63d43fb319a1bcefa63
Fox <you@example.com>
2021-05-28 21:56:52 +0300

    add module with usage algorithms and LZW

--- compression_algorithms/LZW.py
+++ compression_algorithms/LZW.py
@@ -1,15 +1,10 @@
+import base64
 from io import StringIO
 
 
 def lzw_compression(string) -> str:
     """ compression using the Lempel-Ziv-Welch algorithm (LZW). """
-    # chars = set()
-    # for char in string:
-    #     chars.add(char)
-    # dictionary = {val: str(i) for i, val in enumerate(chars)}
-    # print(', '.join([f"'{val}': '{str(i)}'" for i, val in enumerate(sorted(list(chars)))]))
-    # dict_size = len(dictionary)
-    # print(dictionary)
+
     dictionary = {'+':'0', '/':'1', '0':'2', '1':'3', '2':'4', '3':'5',
                   '4':'6', '5':'7', '6':'8', '7':'9', '8':'10', '9':'11',
                   '=':'12', 'A':'13', 'B':'14', 'C':'15', 'D':'16', 'E':'17',
@@ -20,8 +15,8 @@ def lzw_compression(string) -> str:
                   'd':'42', 'e':'43', 'f':'44', 'g':'45', 'h':'46', 'i':'47',
                   'j':'48', 'k':'49', 'l':'50', 'm':'51', 'n':'52', 'o':'53',
                   'p':'54', 'q':'55', 'r':'56', 's':'57', 't':'58', 'u':'59',
-                  'v':'60', 'w':'61', 'x':'62', 'y':'63', 'z':'64'}
-    dict_size = 65
+                  'v':'60', 'w':'61', 'x':'62', 'y':'63', 'z':'64', '\n':65}
+    dict_size = 66
 
     sequence = ""
     compressed_im = []
@@ -38,20 +33,15 @@ def lzw_compression(string) -> str:
     # Output the code for sequence.
     if sequence:
         compressed_im.append(dictionary[sequence])
-    print(f'len file       - {len(string)}')
-    print(f'len compressed - {len(compressed_im)}')
-    print(f'len dictionary - {len(dictionary)}')
-    # print(dictionary)
-    print(type(compressed_im))
     return ' '.join(compressed_im)
 
 
 def lzw_decompression(compressed_photo_str: str) -> object:
     """
-    Reproduction of an image that has been compressed using
+    Reproduction of an file that has been compressed using
     the Lempel-Ziv-Welch (LZW) algorithm.
     """
-    compressed_photo = list(map(int, compressed_photo_str.split(' ')))
+    compressed = list(map(int, compressed_photo_str.split(' ')))
     dictionary = {0:'+', 1:'/', 2:'0', 3:'1', 4:'2', 5:'3',
                   6:'4', 7:'5', 8:'6', 9:'7', 10:'8', 11:'9',
                   12:'=', 13:'A', 14:'B', 15:'C', 16:'D', 17:'E',
@@ -62,69 +52,24 @@ def lzw_decompression(compressed_photo_str: str) -> object:
                   42:'d', 43:'e', 44:'f', 45:'g', 46:'h', 47:'i',
                   48:'j', 49:'k', 50:'l', 51:'m', 52:'n', 53:'o',
                   54:'p', 55:'q', 56:'r', 57:'s', 58:'t', 59:'u',
-                  60:'v', 61:'w', 62:'x', 63:'y', 64:'z'}
-    dict_size = 65
+                  60:'v', 61:'w', 62:'x', 63:'y', 64:'z', 65:'\n'}
+    dict_size = 66
 
     encoded_str = StringIO()
-    sequence = compressed_photo[0]
-    compressed_photo.remove(compressed_photo[0])
-    encoded_str.write(str(sequence))
-    for char in compressed_photo:
-        if char in dictionary:
+    sequence = dictionary[compressed[0]]
+    compressed.remove(compressed[0])
+    encoded_str.write(sequence)
+    for char in compressed:
+        if int(char) in dictionary:
             entry = dictionary[char]
         elif char == dict_size:
-            entry = sequence + sequence[0]
+            entry = str(sequence) + str(sequence)[0]
         else:
             raise ValueError(f'Bad compressed k: {char}')
         encoded_str.write(entry)
 
-        dictionary[dict_size] = sequence + entry[0]
+        dictionary[dict_size] = str(sequence) + entry[0]
         dict_size += 1
         sequence = entry
-        print(sequence)
     encoded_str = encoded_str.getvalue()
-    # encoded_arr = []
-    # rows = 0
-    # encoded_arr.append([])
-    # num = ''
-    # for char in encoded_str:
-    #     if char == ',':
-    #         encoded_arr[rows].append(int(num))
-    #         num = ''
-    #     elif char == '\n':
-    #         encoded_arr[rows].append(int(num))
-    #         encoded_arr.append([])
-    #         rows += 1
-    #         num = ''
-    #     else:
-    #         num += char
-    # arr_2d = [[None] * len(encoded_arr) for _ in range(len(encoded_arr[0]))]
-    # for i, dynamic_arr in enumerate(iter(encoded_arr)):
-    #     for j, val in enumerate(iter(dynamic_arr)):
-    #         arr_2d[i][j] = val
     return encoded_str
-
-
-if __name__ == '__main__':
-    string = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-    compress = lzw_compression(string)
-    print(compress)
-    print(lzw_decompression(compress))
-
-    # diction = {'+':'0', '/':'1', '0':'2', '1':'3', '2':'4', '3':'5',
-    #            '4':'6', '5':'7', '6':'8', '7':'9', '8':'10', '9':'11',
-    #            '=':'12', 'A':'13', 'B':'14', 'C':'15', 'D':'16', 'E':'17',
-    #            'F':'18', 'G':'19', 'H':'20', 'I':'21', 'J':'22', 'K':'23',
-    #            'L':'24', 'M':'25', 'N':'26', 'O':'27', 'P':'28', 'Q':'29',
-    #            'R':'30', 'S':'31', 'T':'32', 'U':'33', 'V':'34', 'W':'35',
-    #            'X':'36', 'Y':'37', 'Z':'38', 'a':'39', 'b':'40', 'c':'41',
-    #            'd':'42', 'e':'43', 'f':'44', 'g':'45', 'h':'46', 'i':'47',
-    #            'j':'48', 'k':'49', 'l':'50', 'm':'51', 'n':'52', 'o':'53',
-    #            'p':'54', 'q':'55', 'r':'56', 's':'57', 't':'58', 'u':'59',
-    #            'v':'60', 'w':'61', 'x':'62', 'y':'63', 'z':'64'}
-    # new_key = []
-    # new_val = []
-    # for key, val in diction.items():
-    #     new_key.append(key)
-    #     new_val.append(val)
-    # print({key:val for key, val in zip(new_val, new_key)})
--- read_file.py
+++ read_file.py
@@ -1,35 +1,95 @@
 import base64
-from compression_algorithms.LZW import lzw_compression
+from compression_algorithms.LZW import lzw_compression, lzw_decompression
 
-with open("example_files/example.mp4", "rb") as videoFile:
-    bits = base64.b64encode(videoFile.read())
-    string = ''
-    for bit in bits:
-        string += chr(bit)
 
-    compress = lzw_compression(string)
+def compress(path: str, path_compressed: str, function: callable):
+    with open(path, "rb") as file_to_compress:
+        bits = base64.b64encode(file_to_compress.read())
+        string = ''
+        for bit in bits:
+            string += chr(bit)
 
-    with open('file_before.txt', 'wb') as file:
-        file.write(bits)
-    with open('file_after.txt', 'w') as file:
-        file.write(' '.join(map(str, compress)))
+    compressed = function(string)
+    with open(path_compressed, 'w') as compressed_file:
+        compressed_file.write(compressed)
 
-    # print(compress)
-    # print(len(bits))
 
-    ###########################################################################
+def decompress(path_compressed: str, path_decompressed: str, function: callable):
+    with open(path_compressed, 'r') as compressed_file:
+        compressed = compressed_file.read()
+    decompressed = lzw_decompression(compressed)
+    with open(path_decompressed, "wb") as file_to_decompress:
+        file_to_decompress.write(base64.b64decode(decompressed))
 
-    # file = open("textTest1.txt", "wb")
-    # file.write(bits)
-    # file.close()
-    # for bit in bits[:10]:
-    #     print(f'{bit} {type(bit)}')
-    # file = open("textTest2.txt", "w")
-    # file.write(string)
-    # file.close()
 
-    ###########################################################################
+if __name__ == '__main__':
+    compress(path='example_files/example.mp4',
+             path_compressed='example_files/tests_files/example_compressed.mp4.txt',
+             function=lzw_compression)
+    decompress(path_compressed='example_files/tests_files/example_compressed.mp4.txt',
+               path_decompressed='example_files/tests_files/example_decompressed.mp4',
+               function=lzw_decompression)
 
-    # fh = open("video.mp4", "wb")
-    # fh.write(base64.b64decode(string))
-    # fh.close()
+
+
+
+
+# with open('file_before.txt', 'wb') as file:
+#     file.write(bits)
+# with open('file_after.txt', 'w') as file:
+#     file.write(' '.join(map(str, compress)))
+# encoded = lzw_decompression(compress)
+# bit_str = str(bits)
+# # print(bits)
+# # print('----------------------------------------------------------')
+# # print(bit_str)
+# decompressed = lzw_decompression(compress)
+# with open("example_files/video.mp4", "wb") as fh:
+#     fh.write(base64.b64decode(decompressed))
+#
+# # decompressed = base64.b64encode(lzw_decompression(compress))
+#
+# # if len(bit_str) == len(encoded):
+# #     for i, val in enumerate(bit_str):
+# #         if val != encoded[i]:
+# #             print(f'suka in {i}')
+# # else:
+# #     print('suka povna')
+# #     print(f'bit_str - {len(bit_str)}\n'
+# #           f'decompressed - {len(decompressed)}')
+# # with open('example_files/encoded_example.mp4', 'wb') as file:
+# #     file.write(decompressed)
+# #
+# # # 6845659
+# # counter = 0
+# # for i in range(336728):
+# #     if decompressed[i] != bit_str[i]:
+# #         print(f'bliat in {i}')
+# #         print(f'decompressed - {decompressed[i]}\n'
+# #               f'bit_str      - {bit_str[i]}')
+# #         counter += 1
+# #         if counter > 10:
+# #             break
+# # else:
+# #     print('pizda')
+#
+#
+# # print(compress)
+# # print(len(bits))
+#
+# ###########################################################################
+#
+# # file = open("textTest1.txt", "wb")
+# # file.write(bits)
+# # file.close()
+# # for bit in bits[:10]:
+# #     print(f'{bit} {type(bit)}')
+# # file = open("textTest2.txt", "w")
+# # file.write(string)
+# # file.close()
+#
+# ###########################################################################
+#
+# # fh = open("video.mp4", "wb")
+# # fh.write(base64.b64decode(string))
+# # fh.close()
