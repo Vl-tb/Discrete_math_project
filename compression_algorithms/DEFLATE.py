adce1f6025cfdc418b5b1d3bc9051ab03ad24fd3
Vadum-cmd <74098778+Vadum-cmd@users.noreply.github.com>
2021-06-01 21:38:35 +0300

    Update DEFLATE.py

--- compression_algorithms/DEFLATE.py
+++ compression_algorithms/DEFLATE.py
@@ -3,14 +3,12 @@ import base64
 
 def deflate(data: str):
     data = data.encode("UTF-8")
-    data = base64.b64encode(data)
     compressed = zlib.compress(data)
     return compressed
 
 
 def inflate(data: bytes):
     decompressed = zlib.decompress(data)
-    decompressed = base64.b64decode(decompressed)
     result = decompressed.decode("UTF-8")
     return result
 
