e24ada66809f26be9d66bafb0b71fa5dbed3e8de
Vadum-cmd <74098778+Vadum-cmd@users.noreply.github.com>
2021-06-02 19:56:05 +0300

    Update DEFLATE.py

--- compression_algorithms/DEFLATE.py
+++ compression_algorithms/DEFLATE.py
@@ -4,13 +4,13 @@ import compression_algorithms.HUFFMAN as huff
 
 def deflate(data):
     lz77_en = compress_massage(data)
-    huff_comp = Huffman_algorithm(lz77_en)
+    huff_comp = huff.Huffman_algorithm(lz77_en)
     deflated = huff_comp.encoding()
     return deflated
 
 
 def inflate(data):
-    huff_comp = Huffman_algorithm(data)
+    huff_comp = huff.Huffman_algorithm(data)
     # huff_comp.binary_tree()
     lz77_en = huff_comp.decoding()
     inflated = decompress_massage(data)
