4106e88081d8afc602fd10715ae0948890565dc8
Vadum-cmd <74098778+Vadum-cmd@users.noreply.github.com>
2021-06-02 19:58:14 +0300

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -16,8 +16,8 @@ def compress(path: str, path_compressed: str, function: callable):
         compressed_file.write(compressed)
 
 
-def decompress(path_compressed: str, path_decompressed: str, function: callable, mode='r'):
-    with open(path_compressed, mode) as compressed_file:
+def decompress(path_compressed: str, path_decompressed: str, function: callable):
+    with open(path_compressed, 'r') as compressed_file:
         compressed = compressed_file.read()
     decompressed = function(compressed)
     with open(path_decompressed, "wb") as file_to_decompress:
@@ -40,5 +40,4 @@ if __name__ == '__main__':
     print("!!!!!!")
     decompress(path_compressed='example_files/tests_files/example_compressed.mp4.txt',
                path_decompressed='example_files/tests_files/example_decompressed.mp4',
-               function=inflate,
-               mode='rb')
+               function=inflate)
