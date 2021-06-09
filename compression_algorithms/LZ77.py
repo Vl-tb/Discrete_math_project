7e174a20e00ec31c64e9aacbebff4684467acfc6
Vadum-cmd <74098778+Vadum-cmd@users.noreply.github.com>
2021-06-09 11:31:44 +0300

    Update LZ77.py

--- compression_algorithms/LZ77.py
+++ compression_algorithms/LZ77.py
@@ -1,107 +1,59 @@
 """ Implementation of LZ77 compression algrorithm """
 
 
-def lz77_compression(src):
+def compress_message(src):
     """
     Compress the given string. Replace similar occurences
     with #copy,steps_back#
     """
-    # MAX_BUFFER = 65536
-    # MAX_BUFFER = 32768
-    # MAX_BUFFER = 4096
-    # MAX_BUFFER = 2048
-    MAX_BUFFER = 100
-    MAX_COPY_LEN = 10
-
     packed_message = ''
 
     run_idx = 0
-    main_len = len(src)
-    cache = {}
-    cache_rev = {}
-    to_del = []
 
     while run_idx < len(src):
-        for what_to_del in to_del:
-            try:
-                for cache_val in cache_rev[what_to_del]:
-                    del cache[cache_val]
-                del cache_rev[run_idx]
-            except KeyError:
-                pass
-        length = min(MAX_COPY_LEN, main_len-run_idx)
+        ln = 9
+        found = False
 
-        while length > 4:
-            message = src[run_idx:run_idx + length]
-            try:
-                copy_idx = cache[message]
-                if copy_idx + length < run_idx:
-                    packed_message += f"#{length},{run_idx - copy_idx}#"
-                    to_del = range(run_idx-MAX_BUFFER, run_idx-MAX_BUFFER+length)
-                    run_idx += length
+        while not found and ln > 3:
+            copy_idx =  run_idx - ln
+
+            while copy_idx >= 0 and (run_idx-copy_idx) < 100:
+                if src[run_idx: run_idx+ln] == src[copy_idx:copy_idx+ln]:
+                    packed_message += f"#{ln}{run_idx-copy_idx}#"
+                    run_idx += ln
+                    found = True
                     break
-                else:
-                    length -= 1
-            except KeyError:
-                cache[message] = run_idx
-                try:
-                    cache_rev[run_idx].add(message)
-                except KeyError:
-                    cache_rev[run_idx] = {message}
-                length -= 1
-        else:
+
+                copy_idx -= 1
+            ln -= 1
+
+        if not found:
             packed_message += src[run_idx]
-            to_del = [run_idx-MAX_BUFFER]
             run_idx += 1
 
     return packed_message
 
-
-def lz77_decompression(encoded_message):
+def decompress_message(encoded_message):
     """
     Decompress the given encoded string. Return the initial file
     in representation of string
     """
-
     unpack = ''
     i_pack = 0
     i_unpack = 0
-
     while i_pack < len(encoded_message):
         if encoded_message[i_pack] != '#':
             unpack += encoded_message[i_pack]
             i_unpack += 1
             i_pack += 1
             continue
-
-        count = 1
-
-        while encoded_message[i_pack + count] != ',':
+        length = int(encoded_message[i_pack+1])
+        count = 3
+        while encoded_message[i_pack+count] != '#':
             count += 1
-        length = int(encoded_message[i_pack + 1:i_pack + count])
-
-        count += 1
-        count_ex = count
-
-        while encoded_message[i_pack + count] != '#':
-            count += 1
-        distance = int(encoded_message[i_pack + count_ex:i_pack + count])
-        unpack += unpack[i_unpack - distance:i_unpack - distance + length]
+        distance = int(encoded_message[i_pack+2:i_pack+count])
+        unpack += unpack[i_unpack-distance:i_unpack-distance+length]
         i_unpack += length
-        i_pack += count + 1
+        i_pack += count+1
 
     return unpack
-
-
-# if __name__ == '__main__':
-#     message0 = "hello hello helloaaaa helloubhirgihrighih hello hello hello"
-
-#     lz_compress = compress_message(message0)
-#     print(f'compress_message - {lz_compress}')
-
-#     print(f'equal - {decompress_message(lz_compress) == message0}')
-#     print(decompress_message(lz_compress))
-#     print(message0)
-
-#     print(len(message0))
-#     print(len(lz_compress))
