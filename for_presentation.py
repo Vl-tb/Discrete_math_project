36f1958e1670b6f58dcce2ec7f568a6d57c4e37f
Vadum-cmd <74098778+Vadum-cmd@users.noreply.github.com>
2021-06-09 11:19:48 +0300

    Update for_presentation.py

--- for_presentation.py
+++ for_presentation.py
@@ -4,6 +4,84 @@ import cv2
 from ffpyplayer.player import MediaPlayer
 
 
+# LZ78 CODING
+
+def lz78_compression(string):
+    new_dict = {}
+    dictionary = {}
+    current_index = 0
+    max_index = 0
+    item = ""
+    for i in range(len(string)):
+        item += string[i]
+        if item not in dictionary:
+            max_index += 1
+            dictionary[item] = (current_index,item[-1],max_index)
+            item = ""
+            current_index = 0
+        else:
+            current_index = dictionary[item][2]
+    if current_index != 0:
+        dictionary['*'] = (current_index,item[-1],max_index)
+
+    for key in dictionary:
+        tup = (dictionary[key][0],dictionary[key][1])
+        new_dict[dictionary[key][2]] = tup
+
+    output = transform(new_dict)
+    return output
+
+def transform(dictionary):
+    output_string = ""
+    for key in dictionary:
+        output_string += str(dictionary[key][0])+str(dictionary[key][1])+" "
+    output_string = output_string[:-1]
+    return output_string
+
+
+def detransform(string):
+    dictionary = {}
+    string = string.split(" ")
+    counter = 1
+    for element in string:
+        dictionary[counter] = (int(element[:-1]),element[-1])
+        counter += 1
+
+    return str(dictionary)
+
+
+def lz78_decompression(compressed_str):
+    compressed = detransform(compressed_str)
+    compressed = eval(compressed)
+    output = ""
+    output1 = ""
+    dictionary = compressed
+
+    for key in dictionary:
+        initial_index = key
+        if initial_index != 0:
+            while initial_index != 0:
+
+                if dictionary[initial_index][0] == 0:
+                    output1 += dictionary[initial_index][1]
+                    break
+
+                output1 += str(dictionary[initial_index][1]) 
+
+                initial_index = dictionary[initial_index][0]
+                
+
+            output +=  output1[::-1]
+            output1 = ""
+
+        elif dictionary[key] == "*":
+            output += dictionary[key][1]
+        else:
+            output += str(dictionary[key][1])
+
+    return output
+
+
 # HUFFMAN CODING
 class Node:
     """
@@ -495,13 +573,19 @@ def main():
         print("Choose the file (enter it's full name).")
         answer_1 = input()
         print("Now choose in which way do you want to do this.")
+        print("lz78")
         print("lz77")
         print("lzw")
         print("huf (Huffman's coding)")
         print("def (DEFLATE)")
         answer_2 = input()
         
-        if answer_2 == "lz77":
+        if answer_2 == "lz78":
+            print("Give this compressed file a name:")
+            answer_3 = input()
+            compress(answer_1, answer_3, lz78_compression)
+
+        elif answer_2 == "lz77":
             print("Give this compressed file a name:")
             answer_3 = input()
             compress(answer_1, answer_3, lz77_compression)
@@ -536,13 +620,19 @@ def main():
         answer_1 = input()
         print("Now choose in which way do you want to do this.")
         print("(make sure the method is the same as it was encoded or it won't work...)")
+        print("lz78")
         print("lz77")
         print("lzw")
         print("huf (Huffman's coding)")
         print("def (DEFLATE)")
         answer_2 = input()
 
-        if answer_2 == "lz77":
+        if answer_2 == "lz78":
+            print("Give this new file a name:")
+            answer_3 = input()
+            decompress(answer_1, answer_3, lz78_decompression)
+
+        elif answer_2 == "lz77":
             print("Give this new file a name:")
             answer_3 = input()
             decompress(answer_1, answer_3, lz77_decompression)
